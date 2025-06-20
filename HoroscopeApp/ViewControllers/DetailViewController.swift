f16410596a90ea16bfccdf1cb397a1277c566333
RafaelLibrero <librerorafael@outlook.com>
2025-06-20 18:30:44 +0200

    Api call and share function

--- HoroscopeApp/ViewControllers/DetailViewController.swift
+++ HoroscopeApp/ViewControllers/DetailViewController.swift
@@ -14,15 +14,20 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var horosocopeDatesLabel: UILabel!
     
     @IBOutlet weak var favoriteMenu: UIBarButtonItem!
+    @IBOutlet weak var horoscopeLuckTextView: UITextView!
+    
+    @IBOutlet weak var loadingView: UIActivityIndicatorView!
     
     var horoscope: Horoscope!
     var isFavorite: Bool = false
-
+    
+    var horoscopeLuck: String? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = horoscope.name
-
+        
         horoscopeNameLabel.text = horoscope.name
         horosocopeDatesLabel.text = horoscope.dates
         horoscopeImageView.image = horoscope.getImage()
@@ -30,6 +35,10 @@ class DetailViewController: UIViewController {
         isFavorite = SessionManager.isFavoriteHoroscope(id: horoscope.id)
         
         setFavoriteImage()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getHoroscopeLuck(period: "daily")
     }
     
     func setFavoriteImage() {
@@ -46,15 +55,68 @@ class DetailViewController: UIViewController {
         setFavoriteImage()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didChangePeriod(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            getHoroscopeLuck(period: "daily")
+        case 1:
+            getHoroscopeLuck(period: "weekly")
+        default:
+            getHoroscopeLuck(period: "monthly")
+        }
     }
-    */
+    
+    func getHoroscopeLuck(period:String) {
+        guard let url = URL(string: "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/\(period)?sign=\(horoscope.id)&day=TODAY") else {
+            return
+        }
+        Task {
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
 
+                guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                    DispatchQueue.main.async {
+                        self.loadingView.isHidden = true
+                    }
+                    return
+                }
+                
+                let jsonData = json["data"] as? [String: String]
+                
+                horoscopeLuck = jsonData?["horoscope_data"] ?? ""
+                
+                DispatchQueue.main.async {
+                    self.horoscopeLuckTextView.text = self.horoscopeLuck
+                    self.loadingView.isHidden = true
+                }
+            } catch {
+                print(error.localizedDescription)
+                DispatchQueue.main.async {
+                    self.loadingView.isHidden = true
+                }
+            }
+        }
+    }
+    
+    @IBAction func share(_ sender: Any) {
+        if let text = horoscopeLuck {
+            let textToShare = [ text ]
+            let activityViewController = UIActivityViewController(activityItems: textToShare, applicationActivities: nil)
+            
+            activityViewController.popoverPresentationController?.sourceView = self.view
+            
+            self.present(activityViewController, animated: true, completion: nil)
+        }
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
